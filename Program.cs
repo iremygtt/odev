ed7c4d25ecc4a0d58a5967f92a36ee7525431337
iremr <iremr@IREM>
2024-12-15 14:44:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("başlangıç değerini giriniz:");
+            int bas = int.Parse(Console.ReadLine());
+            Console.WriteLine("bitiş değerini giriniz:");
+            int bit = int.Parse(Console.ReadLine());
+            Console.WriteLine("tek sayılar mı olsun çift sayılar mı?");
+            string cevap = Console.ReadLine().ToLower();
+
+            if (bas > bit)
+            {
+                int temp = bas;
+                bas = bit;
+                bit = temp;
+            }
+
+            int sonuc = 0;
+            
+            for (int i = bas; i <= bit; i++)
+            
+            {
+                if (i % 2 == 0 && cevap == "çift")
+                {
+                    Console.WriteLine(i);
+                    sonuc += i;
+                }
+
+            else if (i % 2 == 1 && cevap == "tek")
+                {
+                    Console.WriteLine(i);
+                    sonuc += i;
+                }
+                
+                if (i % 2 == (cevap == "tek" ? 1 : 0))
+            
+                {
+                    Console.WriteLine(i);
+                    sonuc += i;
+                }
+            }
+            Console.WriteLine($"Sayıların Toplamı:{sonuc}");
+        }
+    }
+}
